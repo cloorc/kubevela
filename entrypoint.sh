7d2782ee5fdf2d93a020f4b52ee69b325f64a752
zhangh <wittcnezh@foxmail.com>
2022-11-17 20:50:49 +0800

    show startup command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 
 if [ "${1#-}" != "$1" ]; then
     set -- dlv --listen=:2345 --headless=true --log=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --accept-multiclient --api-version=2 exec /usr/local/bin/manager --continue -- "$@"
